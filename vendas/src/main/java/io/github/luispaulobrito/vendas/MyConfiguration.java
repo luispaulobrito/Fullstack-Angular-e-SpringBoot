0c9d80984e83ad03d28fe02b901640b98f320841
luispaulobrito <luiss_paullo@hotmail.com>
2022-11-30 15:00:54 -0300

    classe de configuração criada

---
+++ vendas/src/main/java/io/github/luispaulobrito/vendas/MyConfiguration.java
@@ -0,0 +1,13 @@
+package io.github.luispaulobrito.vendas;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyConfiguration {
+
+    @Bean(name = "applicationName")
+    public String applicationName(){
+        return "Sistemas de Vendas";
+    }
+}
