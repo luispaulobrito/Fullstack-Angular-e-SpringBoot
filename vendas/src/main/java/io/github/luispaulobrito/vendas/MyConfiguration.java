2c96ad6741214d73798d9c367b5244e568564c17
luispaulobrito <luiss_paullo@hotmail.com>
2022-12-02 14:57:46 -0300

    perfis properties de development e production criados

--- vendas/src/main/java/io/github/luispaulobrito/vendas/MyConfiguration.java
+++ vendas/src/main/java/io/github/luispaulobrito/vendas/MyConfiguration.java
@@ -1,10 +1,19 @@
 package io.github.luispaulobrito.vendas;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("development")
 public class MyConfiguration {
 
+    @Bean
+    public CommandLineRunner executar(){
+        return args -> {
+            System.out.println("RODANDO A CONFIGURAÇÃO DE DESENVOLVIMENTO");
+        };
+    }
 
 }
