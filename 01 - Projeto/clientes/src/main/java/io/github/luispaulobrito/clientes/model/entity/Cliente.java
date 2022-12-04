c8df291a26c1c158a570e51bcb8e20e828ded75d
luispaulobrito <luiss_paullo@hotmail.com>
2022-12-03 22:12:32 -0300

    lombok adicionado no projeto

--- 01 - Projeto/clientes/src/main/java/io/github/luispaulobrito/clientes/model/entity/Cliente.java
+++ 01 - Projeto/clientes/src/main/java/io/github/luispaulobrito/clientes/model/entity/Cliente.java
@@ -1,8 +1,13 @@
 package io.github.luispaulobrito.clientes.model.entity;
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDate;
 
 @Entity
+@Data
 public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) //deixa a cargo do banco o autoincremento
