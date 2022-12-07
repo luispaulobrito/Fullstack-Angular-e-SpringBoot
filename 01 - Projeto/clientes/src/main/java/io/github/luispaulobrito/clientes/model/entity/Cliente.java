80935cc882099d48c5d2c07f9667c497a89697dd
luispaulobrito <luiss_paullo@hotmail.com>
2022-12-07 15:08:05 -0300

    correção método salvar cliente e ajuste no formato da data de cadastro

--- 01 - Projeto/clientes/src/main/java/io/github/luispaulobrito/clientes/model/entity/Cliente.java
+++ 01 - Projeto/clientes/src/main/java/io/github/luispaulobrito/clientes/model/entity/Cliente.java
@@ -1,4 +1,5 @@
 package io.github.luispaulobrito.clientes.model.entity;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -22,6 +23,7 @@ public class Cliente {
     private String cpf;
 
     @Column(name = "data_cadastro")
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate dataCadastro;
 
     @PrePersist
