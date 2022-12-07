a429c82956ab9418d09cf9090d764a7661c470e6
luispaulobrito <luiss_paullo@hotmail.com>
2022-12-07 15:58:14 -0300

    adicionado método de buscar cliente por id

--- 01 - Projeto/clientes/src/main/java/io/github/luispaulobrito/clientes/rest/ClienteController.java
+++ 01 - Projeto/clientes/src/main/java/io/github/luispaulobrito/clientes/rest/ClienteController.java
@@ -5,6 +5,7 @@ import io.github.luispaulobrito.clientes.model.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/clientes")
@@ -20,5 +21,10 @@ public class ClienteController {
     @ResponseStatus(HttpStatus.CREATED)
     public Cliente salvar( @RequestBody Cliente cliente){
         return repository.save(cliente);
+    }
+
+    @GetMapping("{id}")
+    public Cliente acharPorId( @PathVariable Integer id){
+        return repository.findById(id).orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 }
