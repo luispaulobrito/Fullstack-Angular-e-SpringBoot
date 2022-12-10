cf02ae32e626dbe8c2e75e663b170a353872ff68
luispaulobrito <luiss_paullo@hotmail.com>
2022-12-10 13:42:35 -0300

    mensagens de erro adicionadas quando nome e cpf estão inválidos ou usuário não encontrado

--- 01 - Projeto/clientes/src/main/java/io/github/luispaulobrito/clientes/rest/ClienteController.java
+++ 01 - Projeto/clientes/src/main/java/io/github/luispaulobrito/clientes/rest/ClienteController.java
@@ -2,6 +2,7 @@ package io.github.luispaulobrito.clientes.rest;
 
 import io.github.luispaulobrito.clientes.model.entity.Cliente;
 import io.github.luispaulobrito.clientes.model.repository.ClienteRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -19,13 +20,13 @@ public class ClienteController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Cliente salvar( @RequestBody Cliente cliente){
+    public Cliente salvar( @RequestBody @Valid Cliente cliente){
         return repository.save(cliente);
     }
 
     @GetMapping("{id}")
     public Cliente acharPorId(@PathVariable Integer id){
-        return repository.findById(id).orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        return repository.findById(id).orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado"));
     }
 
     @DeleteMapping("{id}")
@@ -37,12 +38,12 @@ public class ClienteController {
                     repository.delete(cliente);
                     return Void.TYPE;
                 })
-                .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+                .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado"));
     }
 
     @PutMapping("{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void atualizar( @PathVariable Integer id, @RequestBody Cliente clienteAtualizado){
+    public void atualizar( @PathVariable Integer id, @RequestBody @Valid Cliente clienteAtualizado){
         repository
                 .findById(id)
                 .map( cliente -> {
@@ -50,6 +51,6 @@ public class ClienteController {
                     cliente.setCpf(clienteAtualizado.getCpf());
                     return repository.save(cliente);
                 })
-                .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+                .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado"));
     }
 }
