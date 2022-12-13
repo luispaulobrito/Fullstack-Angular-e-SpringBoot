0c3bf7cc008c71e1d3a7265026d3561c738606ea
luispaulobrito <luiss_paullo@hotmail.com>
2022-12-12 22:24:32 -0300

    adicionado primeiro e segundo component

--- 01 - Projeto/clientes-app/src/app/app.module.ts
+++ 01 - Projeto/clientes-app/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HelloComponent } from "./hello/hello.component";
+import { CursoAngularComponent } from './curso-angular/curso-angular.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent,
+    CursoAngularComponent
   ],
   imports: [
     BrowserModule
