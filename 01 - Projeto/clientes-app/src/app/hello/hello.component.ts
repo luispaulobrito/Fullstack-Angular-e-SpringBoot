7b535a8b80ee59da1d08fda74d83c07ee0387601
luispaulobrito <luiss_paullo@hotmail.com>
2022-12-13 15:44:37 -0300

    template html com array de nomes adicionado

--- 01 - Projeto/clientes-app/src/app/hello/hello.component.ts
+++ 01 - Projeto/clientes-app/src/app/hello/hello.component.ts
@@ -2,10 +2,23 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'hello',
-    template: `
-        <h1>Hello World</h1>
-    `,
+    templateUrl: './hello.component.html'
 })
 export class HelloComponent{
 
+    clientes : Cliente[];
+
+    constructor(){
+        let luis = new Cliente('Luis Paulo',32);
+        let jose = new Cliente('José Vasconcelos',28);
+        let dani = new Cliente('Daniel Oton', 22);
+        this.clientes = [luis, jose, dani];
+    }
+}
+
+class Cliente {
+    constructor(
+      public nome: string,
+      public idade: number
+    ){}
 }
