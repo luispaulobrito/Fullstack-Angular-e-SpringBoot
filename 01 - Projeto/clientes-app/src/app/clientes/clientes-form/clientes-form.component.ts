5bcaf7c0b7448202c7fdd8aef4522f2e286d1c99
luispaulobrito <luiss_paullo@hotmail.com>
2022-12-19 15:55:29 -0300

    formulario de clientes desenvolvido

--- 01 - Projeto/clientes-app/src/app/clientes/clientes-form/clientes-form.component.ts
+++ 01 - Projeto/clientes-app/src/app/clientes/clientes-form/clientes-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Cliente } from '../cliente';
 
 @Component({
   selector: 'app-clientes-form',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./clientes-form.component.css']
 })
 export class ClientesFormComponent {
-
+  cliente: Cliente;
 }
