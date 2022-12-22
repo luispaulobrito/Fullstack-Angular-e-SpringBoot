e22422460623189200d293cd8876baaec382eb8b
luispaulobrito <luiss_paullo@hotmail.com>
2022-12-22 19:12:20 -0300

    salvando clientes atraves da app web no rest api

--- 01 - Projeto/clientes-app/src/app/app.module.ts
+++ 01 - Projeto/clientes-app/src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { ClientesService } from './clientes.service';
 import { ClientesModule } from './clientes/clientes.module';
+
 import { AppRoutingModule } from './app-routing.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { TemplateModule } from './template/template.module';
@@ -15,6 +17,7 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     TemplateModule,
     AppRoutingModule,
     ClientesModule
--- 01 - Projeto/clientes-app/src/app/clientes/clientes-form/clientes-form.component.ts
+++ 01 - Projeto/clientes-app/src/app/clientes/clientes-form/clientes-form.component.ts
@@ -11,12 +11,12 @@ export class ClientesFormComponent {
   cliente: Cliente;
 
   constructor( private service: ClientesService){
-    this.cliente = service.getCliente();
+    this.cliente = new Cliente;
   }
   ngOnInit() : void{
   }
 
   onSubmit(){
-    console.log(this.cliente);
+    this.service.salvar(this.cliente).subscribe( response => {console.log(response)});
   }
 }
