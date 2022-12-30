fb57ef62e508b1a6e47651f80a57ddb1aac75de2
luispaulobrito <luiss_paullo@hotmail.com>
2022-12-29 22:46:34 -0300

    formulário de serviços prestados iniciado com menu para selecionar clientes adicionado

--- 01 - Projeto/clientes-app/src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
+++ 01 - Projeto/clientes-app/src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { ClientesService } from './../../clientes.service';
+import { Cliente } from './../../clientes/cliente';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servico-prestado-form',
   templateUrl: './servico-prestado-form.component.html',
   styleUrls: ['./servico-prestado-form.component.css']
 })
-export class ServicoPrestadoFormComponent {
+export class ServicoPrestadoFormComponent implements OnInit {
 
+  clientes : Cliente[] = []
+
+  constructor(private clienteService: ClientesService){
+
+  }
+
+  ngOnInit(): void {
+    this.clienteService
+      .getClientes()
+      .subscribe( response => this.clientes = response )
+  }
+
+  onSubmit(){
+    console.log("submit");
+
+  }
 }
