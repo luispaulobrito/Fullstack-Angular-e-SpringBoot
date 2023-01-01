4b9ea495287cd3796c3beb7f482c6895e184b60e
luispaulobrito <luiss_paullo@hotmail.com>
2022-12-31 23:47:58 -0300

    variavel de ambiente configurada no environment

--- 01 - Projeto/clientes-app/src/app/app.module.ts
+++ 01 - Projeto/clientes-app/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { ServicoPrestadoService } from './servico-prestado.service';
 import { ServicoPrestadoModule } from './servico-prestado/servico-prestado.module';
 import { ClientesService } from './clientes.service';
 import { ClientesModule } from './clientes/clientes.module';
@@ -25,7 +26,8 @@ import { HomeComponent } from './home/home.component';
     ServicoPrestadoModule
   ],
   providers: [
-    ClientesService
+    ClientesService,
+    ServicoPrestadoService
   ],
   bootstrap: [AppComponent]
 })
