253ef1f38ca86fc53e3a8e7db5b13e83c2f45696
luispaulobrito <luiss_paullo@hotmail.com>
2023-01-27 15:08:52 -0300

    conexao com bd e api criada

--- 02 - Projeto Agenda/agenda-api/src/main/java/io/github/luispaulobrito/agendaapi/AgendaApiApplication.java
+++ 02 - Projeto Agenda/agenda-api/src/main/java/io/github/luispaulobrito/agendaapi/AgendaApiApplication.java
@@ -1,11 +1,16 @@
 package io.github.luispaulobrito.agendaapi;
 
+import io.github.luispaulobrito.agendaapi.model.entity.Contato;
+import io.github.luispaulobrito.agendaapi.model.repository.ContatoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AgendaApiApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AgendaApiApplication.class, args);
 	}
---
+++ 02 - Projeto Agenda/agenda-api/src/main/java/io/github/luispaulobrito/agendaapi/model/entity/Contato.java
@@ -0,0 +1,26 @@
+package io.github.luispaulobrito.agendaapi.model.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Contato {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 150, nullable = false)
+    private String nome;
+
+    @Column(length = 150, nullable = false)
+    private String email;
+
+    @Column
+    private Boolean favorito;
+}
