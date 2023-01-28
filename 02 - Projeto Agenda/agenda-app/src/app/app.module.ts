6065741b8134349a70e52943b44bb864891dcf96
luispaulobrito <luiss_paullo@hotmail.com>
2023-01-27 22:35:37 -0300

    formulario de contatos

--- 02 - Projeto Agenda/agenda-app/src/app/app.module.ts
+++ 02 - Projeto Agenda/agenda-app/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -11,11 +14,14 @@ import { ContatoComponent } from './contato/contato.component';
 @NgModule({
   declarations: [
     AppComponent,
-    ContatoComponent
+    ContatoComponent,
   ],
   imports: [
     BrowserModule,
     MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatInputModule,
     BrowserAnimationsModule
   ],
   providers: [
