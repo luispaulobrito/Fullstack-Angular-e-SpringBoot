5aec2eb2ac572359a7b8b316e41fb637f4ba2f87
luispaulobrito <luiss_paullo@hotmail.com>
2023-01-31 15:57:03 -0300

    favoritando contatos

--- 02 - Projeto Agenda/agenda-app/src/app/contato/contato.component.ts
+++ 02 - Projeto Agenda/agenda-app/src/app/contato/contato.component.ts
@@ -40,6 +40,12 @@ export class ContatoComponent implements OnInit {
     })
   }
 
+  favoritar( contato : Contato){
+    this.service.favorite(contato).subscribe(response => {
+      contato.favorito = !contato.favorito;
+    })
+  }
+
   submit(){
     const formValues = this.formulario.value
     const contato : Contato = new Contato(formValues.nome, formValues.email)
